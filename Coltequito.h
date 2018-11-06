dc03c7fec30b435b3a1bc3c1e821259d7e2462b1
Gueirc <ericmfguimaraes@pm.me>
2018-11-06 19:20:06 -0200

    Consertar declaração de vetores globais.

--- Coltequito.h
+++ Coltequito.h
@@ -9,8 +9,8 @@ typedef struct{
     char descricao[500];
 } corredor;
 
-porta portas[350]; // Qual a porta com maior numero no coltec?
-corredor corredores[100];
+extern porta portas[350]; // Qual a porta com maior numero no coltec?
+extern corredor corredores[100];
 
 // DECLARACAO das fucoes, por algum motivo todo mundo faz isso e nao define elas no .h, mas em outro .c
 void corredor_inciar(); /// Seta todos os corredores e portas
