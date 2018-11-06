03b5cee946cc0e840d67b38c08be2767acd93a30
Gueirc <ericmfguimaraes@pm.me>
2018-11-06 18:38:49 -0200

    Numeros das salas sem numero adicionados, adicionada sala de mecanica.

--- Complexo.c
+++ Complexo.c
@@ -20,17 +20,18 @@ void corredor_iniciar(){
     strcpy(corredores[0].descricao, "\n A portaria tem uma pequena janelinha a direita(0), e na portaria que o porteiro fica e e onde voce pode requisitar chaves para diferentes salas. mais a frente o corredor se divide em tres, pra direita(1), pra esquerda(3) e uma escada pra cima(2)");
     strcpy(portas[0].descricao, "\n E por essa janela a sua direita que você pode perguntar por duvidas e é onde no início do ano os calouros podem ir pra conseguir comprovantes de matricula para, por exemplo, conseguir o meio-passe");
     // TGL 1
-    strcpy(corredores[1].descricao, "\n O corredor se estende ate muito em frente, onde acaba num portao. Esse e o corredor chamado de \"Corredor de tgl\", e pela porta XXX e XXX que voce entra na oficina de tgl. A grande porta dupla vermelha, porta XXX, e o auditorio, a sala XXX e a sala da nuped, XXX, tem uma escada para cima(2) e voce ainda pode voltar(0)");
-    strcpy(portas[/* auditorio */XX].descricao, "\n Esse e o auditorio, lugar onde acontecem as palestras e aulas com palestras, na maioria, no COLTEC. Aqui tem um piano maneiro que voc%c pode tocar, uma vez que a porta estiver aberta, caso nao esteja, voce pode pedir as chaves no SLOP!");
-    strcpy(portas[/*sala do madeira*/XX].descricao, "\n Essa e a sala do Madeira, lugar onde voce tera suas aula de TGL. Ano passado eu tinha aula de 2 em 2 semanas, pois a aula auternava entre a minha subturma (105 - B) e a outra. Aqui temos varias maquinas(cuidado pra nao perder a mao), que com auxilio do Madeira (Alexandre, o professor), podem ser utiliza-das, na aula ou nao. Se voce entregar os materiais e o desenho tecnico, o Madeira pode manufaturar coisas pra voce.");
-    strcpy(portas[/*nuped*/XX].descricao, "\n Eh na sala da nuped que voce pode perguntar sobre qualquer duvida que tenha sobre os horarios das aulas, sera tera aula, o que aconteceu com professores perdidos, alem de informacoes gerais");
+    strcpy(corredores[1].descricao, "\n O corredor se estende ate muito em frente, onde acaba num portao. Esse e o corredor chamado de \"Corredor de tgl\", e pela porta 123 (A ou B) que voce entra na oficina de tgl. A grande porta dupla vermelha, que seria o numero 122, e o auditorio, e a sala 110 e a sala da nuped, tem uma escada para cima(2) e voce ainda pode voltar(0)");
+    strcpy(portas[122].descricao, "\n Esse e o auditorio, lugar onde acontecem as palestras e aulas com palestras, na maioria, no COLTEC. Aqui tem um piano maneiro que voc%c pode tocar, uma vez que a porta estiver aberta, caso nao esteja, voce pode pedir as chaves no SLOP!");
+    strcpy(portas[123].descricao, "\n Essa e a sala do Madeira, lugar onde voce tera suas aula de TGL. Ano passado eu tinha aula de 2 em 2 semanas, pois a aula auternava entre a minha subturma (105 - B) e a outra. Aqui temos varias maquinas(cuidado pra nao perder a mao), que com auxilio do Madeira (Alexandre, o professor), podem ser utiliza-das, na aula ou nao. Se voce entregar os materiais e o desenho tecnico, o Madeira pode manufaturar coisas pra voce.");
+    strcpy(portas[110].descricao, "\n Eh na sala da nuped que voce pode perguntar sobre qualquer duvida que tenha sobre os horarios das aulas, sera tera aula, o que aconteceu com professores perdidos, alem de informacoes gerais");
 
     // Corredor de eletronica 2 (segundo andar)
 
 
     // Corredor que vai pro salao do gremio 3 (que vem da portaria)
-    strcpy(corredores[3].descricao, "\n O corredor tem uma saida para o salao principal (4) e tambem vai para a portaria (0), existem varios escaninhos para alunos guradarem suas coisas, em um cantinho tem uma porta com o numero 102 em cima.");
+    strcpy(corredores[3].descricao, "\n O corredor tem uma saida para o salao principal (4) e tambem vai para a portaria (0), existem varios escaninhos para alunos guradarem suas coisas, masis perto da parede que levav a portaria tem a porta da oficina de mecanica, de numero 105. Em um cantinho, no fundo do corredor, tem uma porta com o numero 102 em cima.");
     strcpy(portas[102].descricao, "\n Eh nessa sala que varias atividades de classe que precisam de um computador podem ocorrer, por exemplo o seu professor de fisica pode precisar de materiais da internet para dar a aula e por isso levaria sua turma para essa sala.");
+    strcpy(portas[105].descricao, "\n Eh aqui que alunos de certos cursos, informatica nao eh um deles, irao trabalhar com materiais feitos de metal e vidro em seu primeiro ano, aqui podem usar as maquinas (como na oficina de tgl) para fabricar coisas.");
 
     // Salao principal onde fica o gremio 4
 
